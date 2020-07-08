f4ec2e62a39bf5e7b934f60e5f2ccf7c6afd3110
Zishan Khan <sendtozishan@gmail.com>
2020-07-08 19:39:07 +0530

    ArrayRotate

---
+++ src/ArraysPS.java
@@ -0,0 +1,23 @@
+public class ArraysPS { //PS= Problem Solving
+    void rotateArray(int[] arr, int d){
+        int temp;
+        for(int i=0; i<d ;i++){
+            temp = arr[0];
+            for(int j=0; j<arr.length-1 ;j++){
+                arr[j] = arr[j+1];
+            }
+            arr[arr.length-1] = temp;
+        }
+
+        for (int a:
+             arr) {
+            System.out.print(a + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArraysPS aps= new ArraysPS();
+        int[] arr= new int[]{1,2,3,4,5,6};
+        aps.rotateArray(arr, 3);
+    }
+}
